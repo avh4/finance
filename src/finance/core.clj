4a74f3f2000bab5cb6c5419b3e22f4212fccf929
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-04 22:09:51 -0700

    Show a retirement chart on the Average American screen.

--- src/finance/core.clj
+++ src/finance/core.clj
@@ -1,6 +1,8 @@
 (ns finance.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn average-income [age profession])
+(defn retirement-chart [title start-age start-income])
+(defn rendering [{age :age profession :profession}]
+  (let [income (average-income age profession)
+        chart-title (str "Average American " age "-year-old " profession)]
+    (retirement-chart chart-title age 32000)))
--- test/finance/core_test.clj
+++ test/finance/core_test.clj
@@ -2,6 +2,9 @@
   (:use [finance.core])
   (:use [midje.sweet]))
 
-(facts "about failing tests"
-  (fact "they fail"
-    (= 0 1) => true ))
+(facts "about the Average American screen"
+  (fact "shows a retirement chart for the selected age and profession"
+    (rendering {:age 30 :profession "Average Joe"}) => (contains ..chart-rendering..)
+    (provided
+      (average-income 30 "Average Joe") => 32000
+      (retirement-chart "Average American 30-year-old Average Joe" 30 32000) => ..chart-rendering..)))
