6823376720da6cd1301107862e24be4a7c7b878b
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-05 20:49:13 -0700

    Create the financial planning model.

--- src/finance/chart.clj
+++ src/finance/chart.clj
@@ -1,14 +1,28 @@
 (ns finance.chart
   (:use [fui drawing]))
 
-
 (defn chart-x-axis
   ([label x-min x-max maj-step] (chart-x-axis label x-min x-max maj-step maj-step))
   ([label x-min x-max maj-step min-step]))
 (defn chart-y-axis [label y-min y-max maj-step])
 (defn chart-line-plot [[x-min x-max] [y-min y-max] data])
 
-(defn retirement-data [start-age start-income end-age])
+(defn apply-inflation [x & _] (* x (Math/exp 0.04)))
+(defn apply-interest [x] (* x (Math/exp 0.07)))
+(defn simulate-year [{start-assets :assets income :income} [age expenses]]
+  (let [withdrawal (if (> age 65) expenses 0)
+        deposit (if (> age 65) 0 income)]
+    {:assets (-> (- start-assets withdrawal) (apply-interest) (+ deposit))
+     :income (-> income (apply-inflation))
+      }))
+(defn retirement-data [start-age start-income end-age]
+  (let [ages (range start-age end-age)
+        tax-rate 0.25
+        savings-rate 0.10
+        salary 32000
+        start-assets 73500 ; for a 30-year-old :: http://mymoneycounselor.com/net-worth-how-are-you-doing
+        expenses (reductions apply-inflation (* salary (- 1 tax-rate savings-rate)) ages)]
+    (map vector ages (map :assets (reductions simulate-year {:assets start-assets :income (* salary savings-rate)} (map vector ages expenses))))))
 (defn max-y-value [numbers])
 (defn round-number [number])
 (defn nice-step [number])
--- test/finance/chart_test.clj
+++ test/finance/chart_test.clj
@@ -4,6 +4,7 @@
   (:use [midje.sweet]))
 
 (def max-age 100)
+(defn round [x] (Math/round (double x)))
 
 (facts "about retirement chart renderings"
   (fact "it has a chart title"
@@ -27,3 +28,9 @@
       (max-y-value (retirement-data ..age.. ..income.. max-age)) => ..max-dollars..
       (round-number ..max-dollars..) => ..max-y..
       (chart-line-plot [..age.. max-age] [0 ..max-y..] (retirement-data ..age.. ..income.. max-age)) => ..assets-plot..)))
+
+(facts "About retirement data"
+  (fact (str "it has a data point for each year from the start age to age " max-age)
+    (map first (retirement-data 30 32000 max-age)) => (range 30 max-age))
+  (fact "it shows the start-of-year retirement assets"
+    (map round (map second (retirement-data 30 32000 max-age))) => [73500 82029 91308 101395 112355 124257 137175 151189 166385 182857 200702 220028 240951 263593 288088 314579 343219 374174 407622 443752 482770 524896 570368 619439 672383 729494 791087 857500 929099 1006274 1089445 1179063 1275613 1379614 1491627 1612250 1742127 1774290 1804942 1833817 1860623 1885040 1906719 1925276 1940293 1951316 1957846 1959341 1955213 1944819 1927461 1902381 1868756 1825691 1772216 1707279 1629739 1538361 1431804 1308621 1167242 1005970 822969 616253 383677 122922 -168516 -493344 -854482 -1255084]))
