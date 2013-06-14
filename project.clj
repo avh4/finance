f9cb1a84c20c4e2b69abca7707d64c96afcf66f9
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-14 12:57:53 -0700

    Switch to deft-0.4.0-SNAPSHOT charts.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lamina "0.5.0-beta9"]
-                 [net.avh4.clj/deft "0.3.0-SNAPSHOT"]]
+                 [net.avh4.clj/deft "0.4.0-SNAPSHOT"]]
   :main finance.app
   :profiles {
     :dev {
--- src/finance/chart.clj
+++ src/finance/chart.clj
@@ -1,14 +1,6 @@
 (ns finance.chart
   (:require [deft [color :as color] [color-basic :as c]])
-  (:use [deft drawing]))
-
-(def axis-color c/black)
-(defn chart-x-axis
-  ([label x-min x-max maj-step] (chart-x-axis label x-min x-max maj-step maj-step))
-  ([label x-min x-max maj-step min-step]
-    [(solid-rect [10 100 300 1] axis-color)]))
-(defn chart-y-axis [label y-min y-max maj-step])
-(defn chart-line-plot [[x-min x-max] [y-min y-max] data])
+  (:use [deft drawing chart]))
 
 (defn apply-inflation [x & _] (* x (Math/exp 0.04)))
 (defn apply-interest [x] (* x (Math/exp 0.07)))
@@ -26,14 +18,14 @@
         start-assets 73500 ; for a 30-year-old :: http://mymoneycounselor.com/net-worth-how-are-you-doing
         expenses (reductions apply-inflation (* salary (- 1 tax-rate savings-rate)) ages)]
     (map vector ages (map :assets (reductions simulate-year {:assets start-assets :income (* salary savings-rate)} (map vector ages expenses))))))
-(defn max-y-value [numbers])
-(defn round-number [number])
-(defn nice-step [number])
+(defn max-y-value [numbers] (apply max (map first numbers)))
+(defn round-number [number] number)
+(defn nice-step [number] (/ number 10))
 
 (defn retirement-chart [title start-age start-income]
   (let [data (retirement-data start-age start-income 100)
         max-y (round-number (max-y-value data))]
     [(drawn-text title 0 10 (color/rgb 0 0 0) "Lucida Grande" 13)
-     (chart-x-axis "Age" start-age 100 1)
-     (chart-y-axis "Retirement assets (USD)" 0 max-y (nice-step max-y))
-     (chart-line-plot [start-age 100] [0 max-y] data)]))
+     (chart-x-axis "Age" start-age 100 1 [0 0 300 200])
+     (chart-y-axis "Retirement assets (USD)" 0 max-y (nice-step max-y) [0 0 300 200])
+     (chart-line-plot [start-age 100] [0 max-y] data [0 0 300 200])]))
